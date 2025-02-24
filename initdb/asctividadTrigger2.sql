bffec05920eb1ce5f5ce1b3ea6d7f9471b04e210
Marc <marc.blanco.barcelo@gmail.com>
2025-02-24 23:13:49 +0100

    final

--- initdb/asctividadTrigger2.sql
+++ initdb/asctividadTrigger2.sql
@@ -1,42 +1,52 @@
-CREATE TABLE Product(
+CREATE TABLE producte(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    nom VARCHAR(255),
     stock INT
-)
+);
 
-CREATE TABLE Order(
+CREATE TABLE comanda(
     id SERIAL PRIMARY KEY,
-    data DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE OrderDetail(
-    id_Order INT REFERENCES ON Order(id),
-    id_Product INT REFERENCES ON Product(id),
-    quantity INT CHECK(quantity >= 0)
-)
+CREATE TABLE linea_comanda(
+    id_producte INT REFERENCES producte(id),
+    id_comanda INT REFERENCES comanda(id),
+    quantitat INT CHECK(quantitat >= 0)
+);
+
+--INSERTS
+
+INSERT INTO producte(nom, stock) VALUES('X', 10);
+INSERT INTO producte(nom, stock) VALUES('Y', 8);
+
+INSERT INTO comanda DEFAULT VALUES;
+INSERT INTO comanda DEFAULT VALUES;
+
 
 --FUNCTIONS
 
-CREATE FUNCTION moreOrderDetail() RETURNS TRIGGER AS $$ BEGIN
-    IF (SELECT stock FROM Product WHERE id = NEW.id_Product < NEW.quantity) THEN
+CREATE FUNCTION nueva_linea_comanda() RETURNS TRIGGER AS $$ BEGIN
+    IF (SELECT stock FROM producte WHERE id = NEW.id_producte) < NEW.quantitat THEN
         RAISE EXCEPTION 'Not enough stock';
         END IF;
-    UPDATE Product SET stock = stock - NEW.quantity WHERE id = NEW.id_Product
+    UPDATE producte SET stock = stock - NEW.quantitat WHERE id = NEW.id_producte;
     RETURN NEW;
     END; $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION lessOrderDetail() RETURNS TRIGGER AS $$ BEGIN
-    UPDATE Product SET stock = stock + OLD.quantity WHERE id = OLD.id_Product
+CREATE FUNCTION quitar_linea_comanda() RETURNS TRIGGER AS $$ BEGIN
+    UPDATE producte SET stock = stock + OLD.quantitat WHERE id = OLD.id_producte;
     RETURN OLD;
     END; $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION updateOrderDetail() RETURNS TRIGGER AS $$ BEGIN
-    UPDATE Product SET stock = stock + OLD.quantity WHERE id = OLD.id_Product
-    UPDATE Product SET stock = stock - NEW.quantity WHERE id = NEW.id_Product
+CREATE FUNCTION cambiar_linea_comanda() RETURNS TRIGGER AS $$ BEGIN
+    UPDATE producte SET stock = stock + OLD.quantitat WHERE id = OLD.id_producte;
+    UPDATE producte SET stock = stock - NEW.quantitat WHERE id = NEW.id_producte;
     RETURN NEW;
     END; $$ LANGUAGE plpgsql;
 
 --TRIGGERS
 
-
-
+CREATE TRIGGER nueva_linea_comandaTRIGGER BEFORE INSERT ON linea_comanda FOR EACH ROW EXECUTE FUNCTION nueva_linea_comanda();
+CREATE TRIGGER quitar_linea_comandaTRIGGER BEFORE DELETE ON linea_comanda FOR EACH ROW EXECUTE FUNCTION quitar_linea_comanda();
+CREATE TRIGGER cambiar_linea_comandaTRIGGER BEFORE UPDATE ON linea_comanda FOR EACH ROW EXECUTE FUNCTION cambiar_linea_comanda();
